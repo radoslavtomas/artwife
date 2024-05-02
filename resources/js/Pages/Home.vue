546789d4840cfd8f4af3c2eb404bcb09636e25e9
Radoslav Tomas <radoslav.tomas@gmail.com>
2024-05-02 20:24:46 +0100

    removed logs

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -23,9 +23,9 @@ data.name = getLocaleVersion(page.name, locale).name
 data.body = getLocaleVersion(page.body, locale).body
 
 onMounted(() => {
-    console.log(data)
-    console.log(edition)
-    console.log(events)
+    // console.log(data)
+    // console.log(edition)
+    // console.log(events)
 })
 
 </script>
