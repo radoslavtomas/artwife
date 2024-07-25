fcebcd3a32d9ccc7bbf3038c1b8b0da50f28fc70
Radoslav Tomas <radoslav.tomas@gmail.com>
2024-07-25 15:31:55 +0100

    added new logos

--- resources/js/Components/Dedication.vue
+++ resources/js/Components/Dedication.vue
@@ -18,15 +18,41 @@ const body = getLocaleVersion(props.page.body, locale).body
 <!--    </div>-->
 
     <!-- LOGOS -->
-    <div id="logos" class="row mt-4 pt-4" style="">
-        <div class="logo col-md-4 col-12" style="">
+    <div id="logos" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 t-4 pt-4" style="">
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
             <img class="" style="width:90%" src="/img/elipsa_logo_napis.svg" />
         </div>
-        <div class="logo col-md-4 col-12" style="">
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
             <img class="" style="width:90%" src="/img/FPU_logo_modre.png" />
         </div>
-        <div class="logo col-md-4 col-12" style="">
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
             <img class="" style="width:90%;" src="/img/orange_foundation_logo.svg" />
+        </div>
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
+            <img class="" style="width:90%;" src="/img/aspekt_logo.png" />
+        </div>
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
+            <img class="" style="width:90%;" src="/img/glosolalia_logo.png" />
+        </div>
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
+            <img class="" style="width:90%;" src="/img/lgpmb_logo.png" />
+        </div>
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
+            <img class="" style="width:90%;" src="/img/moment_logo.png" />
+        </div>
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
+            <img class="" style="width:90%;" src="/img/perform_europe_logo.png" />
+        </div>
+
+        <div class="logo p-4 d-flex justify-content-center align-items-center" style="">
+            <img class="" style="width:90%;" src="/img/ponton_logo.png" />
         </div>
     </div>
 </template>
